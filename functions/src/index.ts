b60791bdf0c53d37200aad10af2067a97d87be85
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2026-01-31 21:52:37 +0200

    europe-west1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,7 +28,10 @@ admin.initializeApp();
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({ 
+  maxInstances: 10,
+  region: "europe-west1"
+});
 
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
